5cf725f62e36065d37cbcb965764e41cca20f347
Mrjoga <163124772+Mrjoga@users.noreply.github.com>
2024-09-23 11:54:03 -0300

    .

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,9 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista"
+
+var Paulo = document.querySelector('#linhaPaulo');
+var pesoPaulo = Paulo.querySelector('.info-peso').textContent;
+var alturaPaulo = Paulo.querySelector('.info-peso').textContent;
+var imcPaulo = pesoPaulo  (alturaPaulo * alturaPaulo);
+Paulo.querySelector('info-imc').textContent = imcPaulo;
